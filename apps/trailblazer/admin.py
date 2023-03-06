9499cb874cbf800a71a877c523cb3f53eea0ebe6
Dylan Bohlender <dylan.bohlender@gmail.com>
2023-03-05 23:05:56 -0700

    Add Trailblazer club tables

--- apps/trailblazer/admin.py
+++ apps/trailblazer/admin.py
@@ -1,3 +1,65 @@
-# from django.contrib import admin
+import logging
 
-# Register your models here.
+from django.contrib import admin
+
+from apps.overrides.admin import AutofillCreatorModelAdmin, linkify
+from apps.trailblazer.models import (
+    TrailblazerTuesdayAttendance,
+    TrailblazerTuesdayReferral,
+    TrailblazerVODReview,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@admin.register(TrailblazerTuesdayAttendance)
+class TrailblazerTuesdayAttendanceAdmin(AutofillCreatorModelAdmin):
+    list_display = (
+        "__str__",
+        linkify("attendee_discord"),
+        "attendance_date",
+        "creator",
+    )
+    list_filter = ("attendee_discord", "attendance_date", "creator")
+    fields = (
+        "attendee_discord",
+        "attendance_date",
+        "creator",
+    )
+
+
+@admin.register(TrailblazerTuesdayReferral)
+class TrailblazerTuesdayReferralAdmin(AutofillCreatorModelAdmin):
+    list_display = (
+        "__str__",
+        linkify("referrer_discord"),
+        linkify("invitee_discord"),
+        "referral_date",
+        "creator",
+    )
+    list_filter = ("referrer_discord", "invitee_discord", "referral_date", "creator")
+    fields = (
+        "referrer_discord",
+        "invitee_discord",
+        "referral_date",
+        "creator",
+    )
+
+
+@admin.register(TrailblazerVODReview)
+class TrailblazerVODReviewAdmin(AutofillCreatorModelAdmin):
+    list_display = (
+        "__str__",
+        linkify("submitter_discord"),
+        "submission_date",
+        "submission_link",
+        "creator",
+    )
+    list_filter = ("submitter_discord", "submission_date", "creator")
+    fields = (
+        "submitter_discord",
+        "submission_date",
+        "submission_link",
+        "submission_notes",
+        "creator",
+    )
--- apps/trailblazer/models.py
+++ apps/trailblazer/models.py
@@ -1,3 +1,77 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from apps.discord.models import DiscordAccount
+from apps.overrides.models import Base
+
+
+class TrailblazerTuesdayAttendance(Base):
+    class Meta:
+        db_table = "TrailblazerTuesdayAttendance"
+        ordering = [
+            "-created_at",
+        ]
+        verbose_name = "Tuesday Attendance"
+        verbose_name_plural = "Tuesday Attendances"
+
+    attendee_discord = models.ForeignKey(
+        DiscordAccount,
+        on_delete=models.RESTRICT,
+        verbose_name="Attendee Discord",
+        related_name="trailblazer_tuesday_attendees",
+    )
+    attendance_date = models.DateField(verbose_name="Attendance Date")
+
+    def __str__(self):
+        return f"{str(self.attendee_discord)} attended on {self.attendance_date}"
+
+
+class TrailblazerTuesdayReferral(Base):
+    class Meta:
+        db_table = "TrailblazerTuesdayReferral"
+        ordering = [
+            "-created_at",
+        ]
+        verbose_name = "Tuesday Referral"
+        verbose_name_plural = "Tuesday Referrals"
+
+    referrer_discord = models.ForeignKey(
+        DiscordAccount,
+        on_delete=models.RESTRICT,
+        verbose_name="Referrer Discord",
+        related_name="trailblazer_tuesday_referrers",
+    )
+    invitee_discord = models.ForeignKey(
+        DiscordAccount,
+        on_delete=models.RESTRICT,
+        verbose_name="Invitee Discord",
+        related_name="trailblazer_tuesday_invitees",
+    )
+    referral_date = models.DateField(verbose_name="Referral Date")
+
+    def __str__(self):
+        return f"{str(self.referrer_discord)} invited {str(self.invitee_discord)}"
+
+
+class TrailblazerVODReview(Base):
+    class Meta:
+        db_table = "TrailblazerVODReview"
+        ordering = [
+            "-created_at",
+        ]
+        verbose_name = "VOD Review"
+        verbose_name_plural = "VOD Reviews"
+
+    submitter_discord = models.ForeignKey(
+        DiscordAccount,
+        on_delete=models.RESTRICT,
+        verbose_name="Submitter Discord",
+        related_name="trailblazer_vod_submitters",
+    )
+    submission_date = models.DateField(verbose_name="Submission Date")
+    submission_link = models.CharField(
+        max_length=512, blank=True, verbose_name="Submission Link"
+    )
+    submission_notes = models.TextField(blank=True, verbose_name="Submission Notes")
+
+    def __str__(self):
+        return f"{str(self.submitter_discord)} submitted on {self.submission_date}"
