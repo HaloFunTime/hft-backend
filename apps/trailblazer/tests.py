fa39914fae08b753ce83519f9bab1f8eae4798c3
Dylan Bohlender <dylan.bohlender@gmail.com>
2023-06-04 16:59:39 -0600

    Add Season 4 Trailblazer Scout challenges

--- apps/trailblazer/tests.py
+++ apps/trailblazer/tests.py
@@ -5,15 +5,19 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from apps.discord.models import DiscordAccount
-from apps.halo_infinite.utils import SEASON_3_START_TIME
+from apps.halo_infinite.utils import SEASON_3_START_TIME, SEASON_4_START_TIME
 from apps.link.models import DiscordXboxLiveLink
 from apps.trailblazer.models import (
+    TrailblazerExcellentVODReview,
     TrailblazerTuesdayAttendance,
     TrailblazerTuesdayReferral,
     TrailblazerVODSubmission,
 )
 from apps.trailblazer.utils import (
     get_s3_discord_earn_dict,
+    get_s4_discord_earn_dict,
+    is_s3_scout_qualified,
+    is_s4_scout_qualified,
     is_scout_qualified,
     is_sherpa_qualified,
 )
@@ -165,69 +169,16 @@ class TrailblazerUtilsTestCase(TestCase):
         )
         self.assertEqual(earn_dict[discord_accounts[0].discord_id]["bookworm"], 0)
 
-    def test_get_s3_xbox_earn_dict(self):
-        # TODO: Write a meaningful unit test for this
-        pass
-
-    @patch("apps.trailblazer.utils.get_csrs")
-    @patch("apps.xbox_live.signals.get_xuid_and_exact_gamertag")
-    def test_is_sherpa_qualified(self, mock_get_xuid_and_exact_gamertag, mock_get_csrs):
-        # Null value provided to method returns False
-        mock_get_csrs.return_value = {"csrs": {}}
-        result = is_sherpa_qualified(None)
-        self.assertEqual(result, False)
-        mock_get_csrs.assert_called_once_with(
-            [None], "edfef3ac-9cbe-4fa2-b949-8f29deafd483"
-        )
-        mock_get_csrs.reset_mock()
-
-        # Create some test data
-        links = []
-        for i in range(10):
-            mock_get_xuid_and_exact_gamertag.return_value = (i, f"test{i}")
-            discord_account = DiscordAccount.objects.create(
-                creator=self.user, discord_id=str(i), discord_tag=f"TestTag{i}#1234"
-            )
-            xbox_live_account = XboxLiveAccount.objects.create(
-                creator=self.user, gamertag=f"testGT{i}"
-            )
-            links.append(
-                DiscordXboxLiveLink.objects.create(
-                    creator=self.user,
-                    discord_account=discord_account,
-                    xbox_live_account=xbox_live_account,
-                    verified=True,
-                )
-            )
-
-        # Returns appropriate value based on whether current reset max CSRs are >= 1650
-        example_csrs = [1433, 1840, 1573, 1222, 780, -1, 1900, 1650, 1649, 1651]
-        for i in range(10):
-            mock_get_csrs.return_value = {
-                "csrs": {
-                    i: {
-                        "current_reset_max_csr": example_csrs[i],
-                    },
-                }
-            }
-            result = is_sherpa_qualified(i)
-            self.assertEqual(result, example_csrs[i] >= 1650)
-            mock_get_csrs.assert_called_once_with(
-                [i],
-                "edfef3ac-9cbe-4fa2-b949-8f29deafd483",
-            )
-            mock_get_csrs.reset_mock()
-
     @patch("apps.trailblazer.utils.get_s3_xbox_earn_dict")
     @patch("apps.xbox_live.signals.get_xuid_and_exact_gamertag")
-    def test_is_scout_qualified(
+    def test_is_s3_scout_qualified(
         self,
         mock_get_xuid_and_exact_gamertag,
         mock_get_s3_xbox_earn_dict,
     ):
         # Null value provided to method returns False
         mock_get_s3_xbox_earn_dict.return_value = {}
-        result = is_scout_qualified(None, None)
+        result = is_s3_scout_qualified(None, None)
         self.assertEqual(result, False)
 
         # Create some test data
@@ -320,5 +271,318 @@ class TrailblazerUtilsTestCase(TestCase):
             discord_id = str(i)
             link = link_for_discord_account.get(discord_id)
             xuid = None if link is None else link.xbox_live_account_id
-            result = is_scout_qualified(discord_id, xuid)
+            result = is_s3_scout_qualified(discord_id, xuid)
             self.assertEqual(result, discord_id in scout_qualified_discord_ids)
+
+    def test_get_s4_discord_earn_dict(self):
+        # Create some test data
+        discord_accounts = []
+        for i in range(2):
+            discord_accounts.append(
+                DiscordAccount.objects.create(
+                    creator=self.user, discord_id=str(i), discord_tag=f"TestTag{i}#1234"
+                )
+            )
+
+        # No IDs = No earn dicts
+        earn_dict = get_s4_discord_earn_dict([])
+        self.assertEqual(earn_dict, {})
+
+        # Max Points - exactly
+        cotcs = []
+        for i in range(5):
+            cotcs.append(
+                TrailblazerTuesdayAttendance.objects.create(
+                    creator=self.user,
+                    attendee_discord=discord_accounts[0],
+                    attendance_date=datetime.date(2023, 7, 6),
+                )
+            )
+        bs = []
+        for i in range(3):
+            bs.append(
+                TrailblazerVODSubmission.objects.create(
+                    creator=self.user,
+                    submitter_discord=discord_accounts[0],
+                    submission_date=datetime.date(2023, 7, 6),
+                )
+            )
+        fcs = []
+        fcs.append(
+            TrailblazerExcellentVODReview.objects.create(
+                creator=self.user,
+                reviewer_discord=discord_accounts[0],
+                review_date=datetime.date(2023, 7, 6),
+            )
+        )
+        earn_dict = get_s4_discord_earn_dict([discord_accounts[0].discord_id])
+        self.assertEqual(
+            earn_dict[discord_accounts[0].discord_id]["church_of_the_crab"], 250
+        )
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["bookworm"], 150)
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["film_critic"], 100)
+
+        # Max points - overages
+        for i in range(2):
+            cotcs.append(
+                TrailblazerTuesdayAttendance.objects.create(
+                    creator=self.user,
+                    attendee_discord=discord_accounts[0],
+                    attendance_date=datetime.date(2023, 7, 6),
+                )
+            )
+            fcs.append(
+                TrailblazerExcellentVODReview.objects.create(
+                    creator=self.user,
+                    reviewer_discord=discord_accounts[0],
+                    review_date=datetime.date(2023, 7, 6),
+                )
+            )
+            bs.append(
+                TrailblazerVODSubmission.objects.create(
+                    creator=self.user,
+                    submitter_discord=discord_accounts[0],
+                    submission_date=datetime.date(2023, 7, 6),
+                )
+            )
+        earn_dict = get_s4_discord_earn_dict([discord_accounts[0].discord_id])
+        self.assertEqual(
+            earn_dict[discord_accounts[0].discord_id]["church_of_the_crab"], 250
+        )
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["bookworm"], 150)
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["film_critic"], 100)
+
+        # Deletion of all records eliminates points
+        for cotc in cotcs:
+            cotc.delete()
+        for fc in fcs:
+            fc.delete()
+        for b in bs:
+            b.delete()
+        earn_dict = get_s4_discord_earn_dict([discord_accounts[0].discord_id])
+        self.assertEqual(
+            earn_dict[discord_accounts[0].discord_id]["church_of_the_crab"], 0
+        )
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["bookworm"], 0)
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["film_critic"], 0)
+
+        # Addition of records outside the date range still results in no points
+        cotcs = []
+        for i in range(5):
+            cotcs.append(
+                TrailblazerTuesdayAttendance.objects.create(
+                    creator=self.user,
+                    attendee_discord=discord_accounts[0],
+                    attendance_date=datetime.date(2023, 4, 6),
+                )
+            )
+        fcs = []
+        fcs.append(
+            TrailblazerExcellentVODReview.objects.create(
+                creator=self.user,
+                reviewer_discord=discord_accounts[0],
+                review_date=datetime.date(2023, 4, 6),
+            )
+        )
+        bs = []
+        for i in range(3):
+            bs.append(
+                TrailblazerVODSubmission.objects.create(
+                    creator=self.user,
+                    submitter_discord=discord_accounts[0],
+                    submission_date=datetime.date(2023, 4, 6),
+                )
+            )
+        earn_dict = get_s4_discord_earn_dict([discord_accounts[0].discord_id])
+        self.assertEqual(
+            earn_dict[discord_accounts[0].discord_id]["church_of_the_crab"], 0
+        )
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["bookworm"], 0)
+        self.assertEqual(earn_dict[discord_accounts[0].discord_id]["film_critic"], 0)
+
+    @patch("apps.trailblazer.utils.get_s4_xbox_earn_dict")
+    @patch("apps.xbox_live.signals.get_xuid_and_exact_gamertag")
+    def test_is_s4_scout_qualified(
+        self,
+        mock_get_xuid_and_exact_gamertag,
+        mock_get_s4_xbox_earn_dict,
+    ):
+        # Null value provided to method returns False
+        mock_get_s4_xbox_earn_dict.return_value = {}
+        result = is_s4_scout_qualified(None, None)
+        self.assertEqual(result, False)
+
+        # Create some test data
+        discord_accounts = []
+        link_for_discord_account = {}
+        xbox_earn_dict = {}
+        for i in range(30):
+            discord_account = DiscordAccount.objects.create(
+                creator=self.user, discord_id=str(i), discord_tag=f"TestTag{i}#1234"
+            )
+            discord_accounts.append(discord_account)
+            # Half of all accounts should get linked gamertags
+            if i % 2 == 0:
+                mock_get_xuid_and_exact_gamertag.return_value = (i, f"test{i}")
+                xbox_live_account = XboxLiveAccount.objects.create(
+                    creator=self.user, gamertag=f"testGT{i}"
+                )
+                link_for_discord_account[str(i)] = DiscordXboxLiveLink.objects.create(
+                    creator=self.user,
+                    discord_account=discord_account,
+                    xbox_live_account=xbox_live_account,
+                    verified=True,
+                )
+                xbox_earn_dict[i] = {
+                    "online_warrior": 0,
+                    "the_cycle": 0,
+                    "checkered_flag": 0,
+                    "them_thar_hills": 0,
+                }
+            # Every second account gets an attendance
+            if i % 2 == 1:
+                TrailblazerTuesdayAttendance.objects.create(
+                    creator=self.user,
+                    attendee_discord=discord_account,
+                    attendance_date=SEASON_4_START_TIME,
+                )
+            # Every third account gets an attendance
+            if i % 3 == 0:
+                TrailblazerTuesdayAttendance.objects.create(
+                    creator=self.user,
+                    attendee_discord=discord_account,
+                    attendance_date=SEASON_4_START_TIME,
+                )
+            # Every fourth account gets an attendance
+            if i % 4 == 0:
+                TrailblazerTuesdayAttendance.objects.create(
+                    creator=self.user,
+                    attendee_discord=discord_account,
+                    attendance_date=SEASON_4_START_TIME,
+                )
+            # Every fifth account gets a VOD review
+            if i % 5 == 0:
+                TrailblazerVODSubmission.objects.create(
+                    creator=self.user,
+                    submitter_discord=discord_account,
+                    submission_date=SEASON_4_START_TIME,
+                )
+            # Every sixth account gets a VOD review
+            if i % 6 == 0:
+                TrailblazerVODSubmission.objects.create(
+                    creator=self.user,
+                    submitter_discord=discord_account,
+                    submission_date=SEASON_4_START_TIME,
+                )
+            # Every seventh account gets an excellent review
+            if i % 7 == 0:
+                TrailblazerExcellentVODReview.objects.create(
+                    creator=self.user,
+                    reviewer_discord=discord_account,
+                    review_date=SEASON_4_START_TIME,
+                )
+            # Every second account earns Online Warrior
+            if i % 2 == 0 and i in xbox_earn_dict:
+                xbox_earn_dict[i]["online_warrior"] = 200
+            # Every third account earns The Cycle
+            if i % 3 == 0 and i in xbox_earn_dict:
+                xbox_earn_dict[i]["the_cycle"] = 100
+            # Every fifth account earns Checkered Flag
+            if i % 6 == 0 and i in xbox_earn_dict:
+                xbox_earn_dict[i]["checkered_flag"] = 100
+            # Every sixth account earns Them Thar Hills
+            if i % 6 == 0 and i in xbox_earn_dict:
+                xbox_earn_dict[i]["them_thar_hills"] = 100
+        mock_get_s4_xbox_earn_dict.return_value = xbox_earn_dict
+
+        # The test data above results in every sixth account clearing the 500 point threshold
+        scout_qualified_discord_ids = {"0", "6", "12", "18", "24"}
+        for i in range(30):
+            discord_id = str(i)
+            link = link_for_discord_account.get(discord_id)
+            xuid = None if link is None else link.xbox_live_account_id
+            result = is_s4_scout_qualified(discord_id, xuid)
+            self.assertEqual(result, discord_id in scout_qualified_discord_ids)
+
+    @patch("apps.trailblazer.utils.is_s4_scout_qualified")
+    @patch("apps.trailblazer.utils.is_s3_scout_qualified")
+    @patch("apps.trailblazer.utils.get_current_season_id")
+    def test_is_scout_qualified(
+        self,
+        mock_get_current_season_id,
+        mock_is_s3_scout_qualified,
+        mock_is_s4_scout_qualified,
+    ):
+        def reset_all_mocks():
+            mock_get_current_season_id.reset_mock()
+            mock_is_s3_scout_qualified.reset_mock()
+            mock_is_s4_scout_qualified.reset_mock()
+
+        # Season 3
+        mock_get_current_season_id.return_value = "3"
+        mock_is_s3_scout_qualified.return_value = True
+        result = is_scout_qualified("123", 123)
+        self.assertEqual(result, True)
+        mock_get_current_season_id.assert_called_once_with()
+        mock_is_s3_scout_qualified.assert_called_once_with("123", 123)
+        mock_is_s4_scout_qualified.assert_not_called()
+        reset_all_mocks()
+
+        # Season 4
+        mock_get_current_season_id.return_value = "4"
+        mock_is_s4_scout_qualified.return_value = True
+        result = is_scout_qualified("123", 123)
+        self.assertEqual(result, True)
+        mock_get_current_season_id.assert_called_once_with()
+        mock_is_s3_scout_qualified.assert_not_called()
+        mock_is_s4_scout_qualified.assert_called_once_with("123", 123)
+        reset_all_mocks()
+
+    @patch("apps.trailblazer.utils.get_csrs")
+    @patch("apps.xbox_live.signals.get_xuid_and_exact_gamertag")
+    def test_is_sherpa_qualified(self, mock_get_xuid_and_exact_gamertag, mock_get_csrs):
+        # Null value provided to method returns False
+        mock_get_csrs.return_value = {"csrs": {}}
+        result = is_sherpa_qualified(None)
+        self.assertEqual(result, False)
+        mock_get_csrs.assert_called_once_with(
+            [None], "edfef3ac-9cbe-4fa2-b949-8f29deafd483"
+        )
+        mock_get_csrs.reset_mock()
+
+        # Create some test data
+        links = []
+        for i in range(10):
+            mock_get_xuid_and_exact_gamertag.return_value = (i, f"test{i}")
+            discord_account = DiscordAccount.objects.create(
+                creator=self.user, discord_id=str(i), discord_tag=f"TestTag{i}#1234"
+            )
+            xbox_live_account = XboxLiveAccount.objects.create(
+                creator=self.user, gamertag=f"testGT{i}"
+            )
+            links.append(
+                DiscordXboxLiveLink.objects.create(
+                    creator=self.user,
+                    discord_account=discord_account,
+                    xbox_live_account=xbox_live_account,
+                    verified=True,
+                )
+            )
+
+        # Returns appropriate value based on whether current reset max CSRs are >= 1650
+        example_csrs = [1433, 1840, 1573, 1222, 780, -1, 1900, 1650, 1649, 1651]
+        for i in range(10):
+            mock_get_csrs.return_value = {
+                "csrs": {
+                    i: {
+                        "current_reset_max_csr": example_csrs[i],
+                    },
+                }
+            }
+            result = is_sherpa_qualified(i)
+            self.assertEqual(result, example_csrs[i] >= 1650)
+            mock_get_csrs.assert_called_once_with(
+                [i],
+                "edfef3ac-9cbe-4fa2-b949-8f29deafd483",
+            )
+            mock_get_csrs.reset_mock()
--- apps/trailblazer/utils.py
+++ apps/trailblazer/utils.py
@@ -7,13 +7,17 @@ from apps.discord.models import DiscordAccount
 from apps.halo_infinite.utils import (
     get_csr_after_match,
     get_csrs,
+    get_current_season_id,
     get_first_and_last_days_for_season,
     get_season_ranked_arena_matches_for_xuid,
 )
 
 logger = logging.getLogger(__name__)
 
+MODE_RANKED_CTF_ID = "507191c6-a492-4331-b2ae-a172101eb23e"
+MODE_RANKED_KOTH_ID = "88c22b1f-2d64-48b9-bab1-26fe4721fb23"
 MODE_RANKED_ODDBALL_ID = "751bcc9d-aace-45a1-8d71-358f0bc89f7e"
+MODE_RANKED_SLAYER_ID = "c2d20d44-8606-4669-b894-afae15b3524f"
 MODE_RANKED_STRONGHOLDS_ID = "22b8a0eb-0d02-4eb3-8f56-5f63fc254f83"
 
 
@@ -140,18 +144,7 @@ def get_s3_xbox_earn_dict(xuids: list[int]) -> dict[int, dict[str, int]]:
     return earn_dict
 
 
-def is_sherpa_qualified(xuid: int) -> bool:
-    # Someone qualifies as a Sherpa if their linked gamertag has a current reset max CSR of 1650 or higher
-    csr = (
-        get_csrs([xuid], "edfef3ac-9cbe-4fa2-b949-8f29deafd483")
-        .get("csrs")
-        .get(xuid, {})
-        .get("current_reset_max_csr", 0)
-    )
-    return csr >= 1650
-
-
-def is_scout_qualified(discord_id: str, xuid: int | None) -> bool:
+def is_s3_scout_qualified(discord_id: str, xuid: int | None) -> bool:
     points = 0
 
     # DISCORD CHALLENGES
@@ -182,3 +175,215 @@ def is_scout_qualified(discord_id: str, xuid: int | None) -> bool:
 
     logger.info(f"Scout Points for {discord_id}: {points}")
     return points >= 500
+
+
+def get_s4_discord_earn_dict(discord_ids: list[str]) -> dict[str, dict[str, int]]:
+    first_day, last_day = get_first_and_last_days_for_season("4")
+    annotated_discord_accounts = DiscordAccount.objects.annotate(
+        attendances=Count(
+            "trailblazer_tuesday_attendees",
+            distinct=True,
+            filter=Q(
+                trailblazer_tuesday_attendees__attendee_discord_id__in=discord_ids,
+                trailblazer_tuesday_attendees__attendance_date__range=[
+                    first_day,
+                    last_day,
+                ],
+            ),
+        ),
+        submissions=Count(
+            "trailblazer_vod_submitters",
+            distinct=True,
+            filter=Q(
+                trailblazer_vod_submitters__submitter_discord_id__in=discord_ids,
+                trailblazer_vod_submitters__submission_date__range=[
+                    first_day,
+                    last_day,
+                ],
+            ),
+        ),
+        excellent_reviews=Count(
+            "trailblazer_excellent_vod_reviewers",
+            distinct=True,
+            filter=Q(
+                trailblazer_excellent_vod_reviewers__reviewer_discord_id__in=discord_ids,
+                trailblazer_excellent_vod_reviewers__review_date__range=[
+                    first_day,
+                    last_day,
+                ],
+            ),
+        ),
+    ).filter(discord_id__in=discord_ids)
+
+    earn_dict = {}
+    for account in annotated_discord_accounts:
+        earn_dict[account.discord_id] = {
+            "church_of_the_crab": min(account.attendances, 5) * 50,  # Max 5 per account
+            "bookworm": min(account.submissions, 3) * 50,  # Max 3 per account
+            "film_critic": min(account.excellent_reviews, 1) * 100,  # Max 1 per account
+        }
+    return earn_dict
+
+
+def get_s4_xbox_earn_dict(xuids: list[int]) -> dict[int, dict[str, int]]:
+    # Get current CSRs for each XUID (needed for calculations)
+    csr_by_xuid = get_csrs(xuids, "edfef3ac-9cbe-4fa2-b949-8f29deafd483").get("csrs")
+
+    earn_dict = {}
+    for xuid in xuids:
+        unlocked_online_warrior = False
+        unlocked_the_cycle = False
+        ctf_wins = 0
+        koth_wins = 0
+
+        # Get matches for this XUID
+        matches = get_season_ranked_arena_matches_for_xuid("4", xuid)
+        matches_sorted = sorted(
+            matches,
+            key=lambda m: datetime.datetime.fromisoformat(
+                m.get("MatchInfo", {}).get("StartTime")
+            ),
+        )
+
+        # Online Warrior: Beat your placement CSR by 200 or more
+        # Validate player has placed and we have at least 5 matches of data for them
+        if csr_by_xuid[xuid]["current_csr"] != -1 and len(matches_sorted) >= 5:
+            placement_match_index = 4
+            placement_csr = None
+            while placement_csr is None:
+                placement_match = matches_sorted[placement_match_index]
+                possible_placement_csr = get_csr_after_match(
+                    xuid, placement_match.get("MatchId")
+                )
+                placement_csr = (
+                    possible_placement_csr if possible_placement_csr != -1 else None
+                )
+                placement_match_index += 1
+            if csr_by_xuid[xuid]["current_reset_max_csr"] >= placement_csr + 200:
+                unlocked_online_warrior = True
+
+        # The Cycle: Win at least one CTF, KotH, Oddball, Slayer, and Strongholds game in a six-hour period
+        sorted_wins = []
+        for match in matches_sorted:
+            if match.get("Outcome") == 2:
+                sorted_wins.append(match)
+        start_index = 0
+        while start_index < len(sorted_wins):
+            ctf_win = False
+            koth_win = False
+            oddball_win = False
+            slayer_win = False
+            strongholds_win = False
+            first_win = sorted_wins[start_index]
+            start_time = datetime.datetime.fromisoformat(
+                first_win.get("MatchInfo", {}).get("StartTime")
+            )
+            six_hours_after_start_time = start_time + datetime.timedelta(hours=6)
+            wins_in_six_hour_window = [first_win]
+            for i in range(start_index + 1, len(sorted_wins)):
+                win = sorted_wins[i]
+                if (
+                    datetime.datetime.fromisoformat(
+                        win.get("MatchInfo", {}).get("EndTime")
+                    )
+                    <= six_hours_after_start_time
+                ):
+                    wins_in_six_hour_window.append(win)
+                else:
+                    break
+            for win in wins_in_six_hour_window:
+                mode_id = (
+                    win.get("MatchInfo", {}).get("UgcGameVariant", {}).get("AssetId")
+                )
+                if mode_id == MODE_RANKED_CTF_ID:
+                    ctf_win = True
+                elif mode_id == MODE_RANKED_KOTH_ID:
+                    koth_win = True
+                elif mode_id == MODE_RANKED_ODDBALL_ID:
+                    oddball_win = True
+                elif mode_id == MODE_RANKED_SLAYER_ID:
+                    slayer_win = True
+                elif mode_id == MODE_RANKED_STRONGHOLDS_ID:
+                    strongholds_win = True
+
+            if ctf_win and koth_win and oddball_win and slayer_win and strongholds_win:
+                unlocked_the_cycle = True
+                break
+            start_index += 1
+
+        # Checkered Flag: Win 25 or more Capture the Flag games
+        # Them Thar Hills: Win 25 or more King of the Hill games
+        for match in matches:
+            if (
+                match.get("MatchInfo", {}).get("UgcGameVariant", {}).get("AssetId")
+                == MODE_RANKED_CTF_ID
+            ):
+                if match.get("Outcome") == 2:
+                    ctf_wins += 1
+            if (
+                match.get("MatchInfo", {}).get("UgcGameVariant", {}).get("AssetId")
+                == MODE_RANKED_KOTH_ID
+            ):
+                if match.get("Outcome") == 2:
+                    koth_wins += 1
+
+        earn_dict[xuid] = {
+            "online_warrior": 200 if unlocked_online_warrior else 0,
+            "the_cycle": 100 if unlocked_the_cycle else 0,
+            "checkered_flag": min(ctf_wins, 25) * 4,
+            "them_thar_hills": min(koth_wins, 25) * 4,
+        }
+    return earn_dict
+
+
+def is_s4_scout_qualified(discord_id: str, xuid: int | None) -> bool:
+    points = 0
+
+    # DISCORD CHALLENGES
+    if discord_id is not None:
+        discord_earn_dict = get_s4_discord_earn_dict([discord_id])
+        earns = discord_earn_dict.get(discord_id)
+
+        # Church of the Crab
+        points += earns.get("church_of_the_crab")
+        # Bookworm
+        points += earns.get("bookworm")
+        # Film Critic
+        points += earns.get("film_critic")
+
+    # XBOX LIVE CHALLENGES
+    if xuid is not None:
+        xbox_earn_dict = get_s4_xbox_earn_dict([xuid])
+        earns = xbox_earn_dict.get(xuid)
+
+        # Online Warrior
+        points += earns.get("online_warrior")
+        # The Cycle
+        points += earns.get("the_cycle")
+        # Checkered Flag
+        points += earns.get("checkered_flag")
+        # Them Thar Hills
+        points += earns.get("them_thar_hills")
+
+    logger.info(f"Scout Points for {discord_id}: {points}")
+    return points >= 500
+
+
+def is_scout_qualified(discord_id: str, xuid: int | None) -> bool:
+    season_id = get_current_season_id()
+    if season_id == "3":
+        return is_s3_scout_qualified(discord_id, xuid)
+    elif season_id == "4":
+        return is_s4_scout_qualified(discord_id, xuid)
+    return False
+
+
+def is_sherpa_qualified(xuid: int) -> bool:
+    # Someone qualifies as a Sherpa if their linked gamertag has a current reset max CSR of 1650 or higher
+    csr = (
+        get_csrs([xuid], "edfef3ac-9cbe-4fa2-b949-8f29deafd483")
+        .get("csrs")
+        .get(xuid, {})
+        .get("current_reset_max_csr", 0)
+    )
+    return csr >= 1650
